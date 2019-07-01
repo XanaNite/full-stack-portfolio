63b3dc8cb72fb4f9a42b96a02aa2dacdef25f7df
Amber Meritt <amber.merittcampbell@gmail.com>
2019-06-30 20:54:34 -0700

    added js scroll, updated html/css

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('js attched');
+
+function animateScroll(){
+    $('.navbar-container').on('click', 'li', function(){
+        const scrollTo = $(this).attr('id');
+        $('html, body').animate({ scrollTop: $(scrollTo).offset().top }, 500);
+    });
+}
+
+$(animateScroll());
